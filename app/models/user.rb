86bfae7dcb833d1afd5b760e0ea16fb9dcaa29b4
mfaiza <mfaizaschool@gmail.com>
2021-02-02 10:17:15 +0700

    Setup UserStock resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+	has_many :user_stocks
+	has_many :stocks, through: :user_stocks
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable, :registerable,
+    	:recoverable, :rememberable, :validatable
 end
