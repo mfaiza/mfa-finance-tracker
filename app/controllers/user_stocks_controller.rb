a1a3be9617cf81918d615307ae201bbdf7409a09
mfaiza <mfaizaschool@gmail.com>
2021-02-11 11:51:36 +0700

    add additional fields

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,12 @@ class UserStocksController < ApplicationController
 		redirect_to portfolio_path
 	end
 
+	def destroy
+		stock = Stock.find(params[:id])
+		user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+		user_stock.destroy
+		flash[:notice] = "#{stock.ticker} was successfully deleted!"
+		redirect_to portfolio_path
+	end
+
 end
