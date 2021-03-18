9edcae9cea8a717fb3b491685d9c1d093d672067
mfaiza <mfaizaschool@gmail.com>
2021-03-18 15:52:53 +0700

    display search friend result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,30 @@ class UsersController < ApplicationController
   def portfolio
   	@trackedStocks = current_user.stocks
   end
+
+  def my_friends
+  	@friends = current_user.friends
+  end
+
+  def search
+  	if params[:friend].present?
+		@friends = User.search(params[:friend])
+		@friends = current_user.except_current_user(@friends)
+		if @friends
+			respond_to do |format|
+				format.js { render partial: 'users/friend_result'}
+			end
+		else
+			respond_to do |format|
+				flash.now[:alert] = "User not found!"
+				format.js { render partial: 'users/friend_result'}
+			end
+		end
+	else
+		respond_to do |format|
+				flash.now[:alert] = "Please enter Name or Email user to Search!"
+				format.js { render partial: 'users/friend_result'}
+			end
+		end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,5 +49,13 @@ class User < ApplicationRecord
     def self.matches(field_name, param)
         where("#{field_name} like ?", "%#{param}%")
     end
+    
+    def except_current_user(users)
+        users.reject { |user| user.id == self.id}
+    end
+
+    def not_friends_with?(friend_id)
+        !self.friends.where(id: friend_id).exists?
+    end
 
 end
